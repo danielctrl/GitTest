fda9d28a6447be762a853133d1a01acd2ff44acd
Daniel Moreira <daniel.moreira@thomsonreuters.com>
2019-02-06 12:02:52 -0200

    First commit

---
+++ App/ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestAdd()
+		{
+			int expected = 5;
+			int a = 3;
+			int b = 2;
+
+
+			int actual = Program.Sum(a, b);
+
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void BrokenTestAdd()
+		{
+			int expected = 6;
+			int a = 3;
+			int b = 2;
+
+
+			int actual = Program.Sum(a, b);
+
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
